9793ec955f41fb900c34391c45e45cd1151a6be3
tabufx50 <44902903+tabufx50@users.noreply.github.com>
2021-03-15 08:33:52 -0500

    added dependencies and stripe code

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_AmIQ1UOpnSuSdZwnj5QK3sgA004yob5kyW';
 
   const onToken = token => {
     console.log(token);
